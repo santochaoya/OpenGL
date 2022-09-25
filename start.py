ccee554bee46e22f36fe59edaac758e7250f55b2
XimeCraft <santochaoya@yahoo.com.hk>
2022-09-25 20:56:06 +0800

    create Triangle

---
+++ start.py
@@ -0,0 +1,107 @@
+from subprocess import CompletedProcess
+import pygame as pg
+from OpenGL.GL import *
+import os
+import numpy as np
+from OpenGL.GL.shaders import compileProgram, compileShader
+
+
+class App:
+
+    def __init__(self) -> None:
+        
+        pg.init()
+        os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (2700, 500)
+
+        pg.display.set_mode((640, 480), pg.OPENGL|pg.DOUBLEBUF)
+
+        self.clock = pg.time.Clock()
+
+        # initialize opengl
+        glClearColor(0.1, 0.2, 0.2, 1)
+
+        self.triangle = Triangle()
+        self.shader = self.createShader('shaders/vertex.txt', 'shaders/fragment.txt')
+        glUseProgram(self.shader)
+
+        self.mainloop()
+
+    def createShader(self, vertexFilepath, fragmentFilepath):
+        
+        with open(vertexFilepath, 'r') as f:
+            vertex_src = f.readlines()
+
+        with open(fragmentFilepath, 'r') as f:
+            fragment_src = f.readlines()
+
+        shader = compileProgram(
+            compileShader(vertex_src, GL_VERTEX_SHADER),
+            compileShader(fragment_src, GL_FRAGMENT_SHADER)
+        )
+
+        return shader
+
+    def mainloop(self):
+
+        running = True
+
+        while (running):
+            # check events
+            for event in pg.event.get():
+                if (event.type == pg.QUIT):
+                    running = False
+                
+            # refresh screen
+            glClear(GL_COLOR_BUFFER_BIT)
+
+            # draw the triangle
+            glUseProgram(self.shader)
+            glBindVertexArray(self.triangle.vao)
+            glDrawArrays(GL_TRIANGLES, 0, self.triangle.vertex.count)
+
+            pg.display.flip()
+
+            # timing
+            self.clock.tick(60)
+        self.quit()
+
+    def quit(self):
+
+        self.triangle.destroy()
+        glDeleteProgram(self.shader)
+        pg.quit()
+
+class Triangle:
+
+    def __init__(self):
+        
+        # set x, y, z, r, g, b
+        self.vertices = (
+            -0.5, -0.5, 0, 1, 0, 0,
+            0.5, -0.5, 0, 0, 1, 0,
+            0, 0.5, 0, 0, 0, 1
+        )
+
+        self.vertices = np.array(self.vertices, dtype=np.float32)
+
+        self.vertex_count = 3
+  
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        glBufferData(GL_ARRAY_BUFFER, self.vertices.nbytes, self.vertices, GL_STATIC_DRAW)
+
+        # Describe the attributes in the vbo
+        glEnableVertexAttribArray(0)
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(0))
+        glEnableVertexAttribArray(1)
+        glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 24, ctypes.c_void_p(12))
+     
+    def destroy(self):
+ 
+        glDeleteVertexArrays(1, (self.vao, ))
+        glDeleteBuffers(1, (self.vbo, ))
+
+if __name__ == '__main__':
+    myApp = App()
